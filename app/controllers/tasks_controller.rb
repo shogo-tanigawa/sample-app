17a1f05704c549ab4ea8189c7891fa22ee041eee
tanigawa <kurohamu2525@gmail.com>
2022-07-12 15:15:33 +0000

    7月12日C5 90%

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
   
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+  
   def index
+     @tasks = Task.all
+  end
+  
+  def show
+    @user = User.find(params[:user_id])
+    @task = @user.tasks
   end
 end
