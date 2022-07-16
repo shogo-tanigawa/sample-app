3b5dcbecb8c33534cea771d27e1b5e82df06cfe4
tanigawa <kurohamu2525@gmail.com>
2022-07-16 13:15:40 +0000

    C6 task新規作成機能実装途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,18 +5,20 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(description: params[:description])
+    @task = Task.new
+    @task.name = params[:name]
+    @task.description = params[:description]
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
-      redirect_to user_tasks_url @user
+      redirect_to user_tasks_url
     else
       render :new
     end
   end
   
   def index
-     @tasks = Task.all
-     @user = User.find(params[:user_id])
+    @tasks = Task.all
+    @user = User.find(params[:user_id])
   end
   
   def show
