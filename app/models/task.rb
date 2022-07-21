8988ed8483251046e643722e0be36c051e11ce01
tanigawa <kurohamu2525@gmail.com>
2022-07-21 15:15:09 +0000

    C10,C11完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+  
   belongs_to :user
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 100 }
 end
