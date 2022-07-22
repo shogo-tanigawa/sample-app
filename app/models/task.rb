44181449ef88bdd7000c75bdef5c6adda1f78188
tanigawa <kurohamu2525@gmail.com>
2022-07-22 12:09:40 +0000

    C12 日本語化完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,5 @@ class Task < ApplicationRecord
   belongs_to :user
   
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { in: 5..300 }
 end
